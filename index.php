855287f9f0c7b5a0bfee98d7ffade164ac77619a
Mixeration <mixeration@gmail.com>
2022-09-06 21:16:13 +0300

    0.0.1

--- index.php
+++ index.php
@@ -1,5 +1,25 @@
-<?php
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="assets/css/PageType$Login.css" rel="stylesheet" type="text/css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="login">
+        <div class="box">
+            <h1>Login</h1>
+            <label for="username">Please enter your username</label>
+            <input id="username" type="text">
+            <label for="password">Please enter your password</label>
+            <input id="password" type="password">
+        </div>
+    </div>
+    <div class="about-box">
 
-echo "Selam";
-
-?>
+    </div>
+</body>
+</html>
